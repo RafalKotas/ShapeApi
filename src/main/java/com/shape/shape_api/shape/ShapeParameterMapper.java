5494c2d6be64cfd432d71f597169c5e9ec30e95a
Raffel222 <Raffel222@gmail.com>
2025-04-13 19:48:42 +0200

    Remove comment

--- src/main/java/com/shape/shape_api/shape/ShapeParameterMapper.java
+++ src/main/java/com/shape/shape_api/shape/ShapeParameterMapper.java
@@ -3,6 +3,6 @@ package com.shape.shape_api.shape;
 import java.util.Map;
 
 public interface ShapeParameterMapper<T> {
-    String getKey(); // np. "v1:square"
+    String getKey();
     T map(Map<String, Long> parameters);
 }

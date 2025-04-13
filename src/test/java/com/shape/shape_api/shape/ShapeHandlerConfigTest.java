43cee239f9e03231b84bc8f1f0f3b9be9868786a
Raffel222 <Raffel222@gmail.com>
2025-04-13 21:44:12 +0200

    ShapeHandlerConfig unit tests

---
+++ src/test/java/com/shape/shape_api/shape/ShapeHandlerConfigTest.java
@@ -0,0 +1,53 @@
+package com.shape.shape_api.shape;
+
+import com.shape.shape_api.circle.v1.CircleHandlerV1;
+import com.shape.shape_api.circle.v2.CircleHandlerV2;
+import com.shape.shape_api.rectangle.v1.RectangleHandlerV1;
+import com.shape.shape_api.rectangle.v2.RectangleHandlerV2;
+import com.shape.shape_api.square.v1.SquareHandlerV1;
+import com.shape.shape_api.square.v2.SquareHandlerV2;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ShapeHandlerConfigTest {
+
+    @Test
+    void shouldCreateShapeHandlerMapWithCorrectKey() {
+        // given
+        CircleHandlerV1 circleHandlerV1 = mock(CircleHandlerV1.class);
+        CircleHandlerV2 circleHandlerV2 = mock(CircleHandlerV2.class);
+        SquareHandlerV1 squareHandlerV1 = mock(SquareHandlerV1.class);
+        SquareHandlerV2 squareHandlerV2 = mock(SquareHandlerV2.class);
+        RectangleHandlerV1 rectangleHandlerV1 = mock(RectangleHandlerV1.class);
+        RectangleHandlerV2 rectangleHandlerV2 = mock(RectangleHandlerV2.class);
+
+        when(circleHandlerV1.getKey()).thenReturn("v1:circle");
+        when(circleHandlerV2.getKey()).thenReturn("v2:circle");
+        when(squareHandlerV1.getKey()).thenReturn("v1:square");
+        when(squareHandlerV2.getKey()).thenReturn("v2:square");
+        when(rectangleHandlerV1.getKey()).thenReturn("v1:rectangle");
+        when(rectangleHandlerV2.getKey()).thenReturn("v2:rectangle");
+
+        ShapeHandlerConfig config = new ShapeHandlerConfig();
+
+        // when
+        Map<String, ShapeHandler<?, ?>> result = config.shapeHandlers(
+                circleHandlerV1,
+                squareHandlerV1,
+                rectangleHandlerV1,
+                circleHandlerV2,
+                squareHandlerV2,
+                rectangleHandlerV2
+        );
+
+        // then
+        assertEquals(6, result.size());
+        assertEquals(circleHandlerV1, result.get("v1:circle"));
+        assertEquals(squareHandlerV2, result.get("v2:square"));
+    }
+}

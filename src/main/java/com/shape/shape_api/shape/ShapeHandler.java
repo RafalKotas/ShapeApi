85f68fc2be48263c8c6a8175a5e9101272c222b3
Raffel222 <Raffel222@gmail.com>
2025-04-23 17:38:33 +0200

    Handler params convention

--- src/main/java/com/shape/shape_api/shape/ShapeHandler.java
+++ src/main/java/com/shape/shape_api/shape/ShapeHandler.java
@@ -4,8 +4,8 @@ import com.shape.shape_api.model.Shape;
 
 import java.util.List;
 
-public interface ShapeHandler<IN_DTO, ENTITY extends Shape> {
+public interface ShapeHandler<DTO, ENTITY extends Shape> {
     String getKey();
     List<ENTITY> getAllShapes();
-    ENTITY createShape(IN_DTO dto);
+    ENTITY createShape(DTO dto);
 }
